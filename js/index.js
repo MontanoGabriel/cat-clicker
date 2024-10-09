5379acf1c202ed8eccfb781b3734a107b2a6889f
a22jhepincre <a22jhepincre@inspedralbes.cat>
2024-10-09 13:00:41 +0200

    prueva

---
+++ js/index.js
@@ -0,0 +1,44 @@
+let htmlstring;
+/*let data  =
+  [
+    {
+        "nombre": "Miau",
+        "imagen": "https://ejemplo.com/imagen1.jpg",
+        "toques": 0
+    },
+    {
+        "nombre": "Pelusa",
+        "imagen": "https://ejemplo.com/imagen2.jpg",
+        "toques": 0
+    },
+    {
+        "nombre": "Gatito",
+        "imagen": "https://ejemplo.com/imagen3.jpg",
+        "toques": 0
+    },
+    {
+        "nombre": "Nube",
+        "imagen": "https://ejemplo.com/imagen4.jpg",
+        "toques": 0
+    },
+    {
+        "nombre": "Sombra",
+        "imagen": "https://ejemplo.com/imagen5.jpg",
+        "toques": 0
+    }
+];
+
+for(let j=0; j < data.length;j++){
+
+}*/
+
+
+let cont=69;
+let elem = document.getElementById('gato');
+elem.addEventListener('click', contador());
+
+function contador(){
+  cont ++;
+  document.getElementById=('cont').innerHTML =cont;
+  
+}
